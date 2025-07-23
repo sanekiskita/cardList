e9198831fdcdd023a846145aa987f86e4364c112
alex <aleshev@nevalink.biz>
2025-07-23 23:33:35 +0300

    logic transfer

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,18 +1,98 @@
 <script setup lang="ts">
+    import { ref, onMounted, watch, computed, defineAsyncComponent, type Ref, type Component } from 'vue';
+
+    import type { responseQuotes, Quote } from '../interface/IQuotes';
+    import type { responseTodos,  Todo} from '../interface/ITodos';
+
+    import ApiDummyjson from '../services/ApiDummyjson';
+    import config from '../configs/config';
+    import loadStore from '../stores/load'
+
     import pageList from './ThePagination.vue';
 
-    const {maxPage, modelValue: selectedPage, } = defineProps<{
-        maxPage: number,
+    const CardTodo = defineAsyncComponent(() =>
+        import('../components/CardTodo.vue')
+    );
+    const CardQuote = defineAsyncComponent(() =>
+        import('../components/CardQuote.vue')
+    );
+
+    const {type, modelValue: selectedPage, } = defineProps<{
+        type: 'todos'|'quotes',
         modelValue: number,
     }>();
 
     const emit = defineEmits<{
-        (e: 'update:modelValue', value: number): void
-    }>()
+        (e: 'update:modelValue', value: number): void,
+    }>();
+
+    const load = loadStore();
+
+    const total = ref(0);
+    const maxPage = ref(1);
+    const source: Ref<null|responseTodos|responseQuotes> = ref(null);
+
+    const Api = new ApiDummyjson();
+
+    const setCardInfo = async () => {
+        const { CARD_LIMIT } = config
+
+        load.loadStart();
+
+        try {
+            source.value = await Api.get(ApiDummyjson.getUrlForGet(type, selectedPage));
+        } catch (errorCode) {
+            alert(errorCode);
+            return;
+        }
+
+        total.value = source.value.total;
+        maxPage.value = CARD_LIMIT > 0
+            ? Math.ceil(total.value / CARD_LIMIT)
+            : 1;
+        load.loadStop();
+    };
+
+    watch(() => type, () => {
+        source.value = null;
+        setCardInfo();
+    });
+
+    watch(() => selectedPage, () => {
+        source.value = null;
+        setCardInfo();
+    });
+
+    onMounted(() => {
+        setCardInfo();
+    });
+
+    const cardElements = computed(() => {
+        if (source.value === null) {
+            return {};
+        }
+
+        if (type === 'todos' && type in source.value) {
+            return {
+                comp: CardTodo as Component<{ card: Todo }>,
+                items: source.value.todos
+            };
+        }
+
+        if (type === 'quotes' && type in source.value) {
+            return {
+                comp: CardQuote as Component<{ card: Quote }>,
+                items: source.value.quotes
+            };
+        }
+
+        return {};
+    });
 </script>
 
 <template>
     <div class="card_list">
+        <p>Записей всего: {{total}}</p>
         <div class="card_list_pagination" >
             <page-list
                 :max-page="maxPage"
@@ -21,7 +101,19 @@
             />
         </div>
         <div class="card_list__content">
-            <slot></slot>
+            <template v-if="cardElements.items?.length">
+                <component v-for="card in cardElements.items"
+                    class="card"
+                    :is="cardElements.comp"
+                    :key="card.id"
+                    :card="card"
+                />
+            </template>
+            <div v-else
+                class="__empty"
+            >
+                {{cardElements.items ? 'нет записей на текущей странице' : 'обновление...'}}
+            </div>
         </div>
         <div class="card_list_pagination">
             <page-list
@@ -45,6 +137,11 @@
         display: grid;
         grid-template-columns: repeat(3, 1fr);
         gap: var(--gap-elements-block);
+    }
+
+    .__empty {
+      grid-column: 1 / -1;
+      text-align: center;
     }
 
     @media (max-width: 900px) {
--- src/views/SourceView.vue
+++ src/views/SourceView.vue
@@ -1,116 +1,33 @@
 <script setup lang="ts">
-  import { ref, onMounted, watch, computed, defineAsyncComponent, type Ref, type Component } from 'vue';
+  import { ref, watch, type Ref } from 'vue';
   import { useRouter, useRoute } from 'vue-router';
-
-  import type { responseQuotes, Quote } from '../interface/IQuotes';
-  import type { responseTodos,  Todo} from '../interface/ITodos';
-
-  import ApiDummyjson from '../services/ApiDummyjson';
-  import config from '../configs/config';
-  import loadStore from '../stores/load'
 
   import cardList from '../components/CardList.vue';
 
-  const CardTodo = defineAsyncComponent(() =>
-    import('../components/CardTodo.vue')
-  )
-  const CardQuote = defineAsyncComponent(() =>
-    import('../components/CardQuote.vue')
-  )
-
   const curRoute = useRoute();
   const router = useRouter();
-  const load = loadStore();
 
   const type: Ref<string|'todos'|'quotes'> = ref('todos');
-  const total = ref(0);
-  const maxPage = ref(1);
   const selectedPage = ref(1);
-  const source: Ref<null|responseTodos|responseQuotes> = ref(null);
-
-  const Api = new ApiDummyjson();
-
-  const setSourse = async () => {
-    const { CARD_LIMIT } = config
-
-    load.loadStart();
-    try {
-      source.value = await Api.get(ApiDummyjson.getUrlForGet(type.value, selectedPage.value));
-    } catch (errorCode) {
-      alert(errorCode);
-      return;
-    }
-    total.value = source.value.total;
-    maxPage.value = CARD_LIMIT > 0
-        ? Math.ceil(total.value / CARD_LIMIT)
-        : 1;
-    load.loadStop();
-  };
-
-  watch(type, () => {
-    selectedPage.value = +curRoute.params?.page || 1;
-    source.value = null;
-    setSourse();
-  });
 
   watch(selectedPage, (newVal) => {
-    setSourse();
     router.push({params: {page: `${newVal}`}});
   });
 
   watch(curRoute, () => {
     type.value = String(curRoute.name);
-  });
-
-  onMounted(() => {
-    type.value = String(curRoute.name);
-    
-    setSourse().then(() => {
-      selectedPage.value = +(+curRoute.params?.page > +maxPage.value ? maxPage.value : (curRoute.params?.page || 1))
-    });
-  });
-
-  const sourceElements = computed(() => {
-    if (source.value === null) {
-      return {};
-    }
-
-    if (type.value === 'todos' && type.value in source.value) {
-      return {
-        comp: CardTodo as Component<{ card: Todo }>,
-        items: source.value.todos
-      };
-    }
-
-    if (type.value === 'quotes' && 'quotes' in source.value) {
-      return {
-        comp: CardQuote as Component<{ card: Quote }>,
-        items: source.value.quotes
-      };
-    }
-
-    return {};
+    selectedPage.value = +curRoute.params?.page || 1;
   });
 </script>
 
 <template>
-  <main v-if="source" class="source">
-    <p>Записей всего: {{total}}</p>
-    <card-list class="source__content" :max-page="maxPage" v-model="selectedPage">
-      <template v-if="sourceElements.items?.length">
-        <component v-for="card in sourceElements.items"
-          class="card"
-          :is="sourceElements.comp"
-          :key="card.id"
-          :card="card"
-        />
-      </template>
-      <div v-else
-        class="__empty"
-      >
-        нет записей на текущей странице
-      </div>
-    </card-list>
+  <main class="source">
+    <card-list
+      v-if="type === 'todos' || type === 'quotes'"
+      class="source__content"
+      :type="type"
+      v-model="selectedPage"
+    />
   </main>
 </template>
 
@@ -120,16 +37,7 @@
       flex-direction: column;
     }
 
-    .source > *:not(:last-child) {
-      margin-bottom: var(--gap-elements-block);
-    }
-
     .source__content {
       flex: 1;
-    }
-
-    .__empty {
-      grid-column: 1 / -1;
-      text-align: center;
     }
 </style>
